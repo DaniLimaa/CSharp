b3d5aa821ae8f72015050bc8bac1a4b091439f9e
Daniel Camargo <169094692+DaniLimaa@users.noreply.github.com>
2024-11-18 11:56:27 -0300

    Add files via upload

---
+++ RoupaBox/RoupaBox/CadastroPro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoupaBox
+{
+    internal class CadastroPro
+    {
+        public void cadProdutos()
+        {
+            Console.Clear();
+            Console.WriteLine("Cadastro Produtos");
+            Console.WriteLine("\nDigite a marca do produto");
+            string marcaPro = Console.ReadLine();
+            Console.WriteLine("\nDigite o tamanho do produto");
+            string tamPro = Console.ReadLine();
+            Console.WriteLine("\nDigite a cor do produto");
+            string corPro = Console.ReadLine();
+            Console.WriteLine("\nDigite a categoria do produto");
+            string catPro = Console.ReadLine();
+            Console.WriteLine("\nDigite a descrição do produto");
+            string descPro = Console.ReadLine();
+            Console.WriteLine("\nDigite o valor do produto");
+            double valPro = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nDigite o peso do produto");
+            double pesoPro = double.Parse(Console.ReadLine());
+            Console.WriteLine("Produto cadastrado com secesso");
+        }
+    }
+}
---
+++ RoupaBox/RoupaBox/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoupaBox
+{
+    internal class Menu
+    {
+        public void exibirMenu()
+        {
+            Console.WriteLine("\nDigite 0 para Sair");
+            Console.WriteLine("\nDigite 1 Cadastro de Clientes");
+            Console.WriteLine("\nDigite 2 Listar Clientes");
+            Console.WriteLine("\nDigite 3 Cadastro de Produtos");
+            Console.WriteLine("\nDigite 4 Listar Produtos");
+            Console.WriteLine("\nEscolha uma opçao");
+            int opcoesMenu = int.Parse(Console.ReadLine());
+
+            switch (opcoesMenu)
+            {
+                case 0:
+
+                    break;
+                    
+                case 1:
+                    CadastroCli cadastroCli = new CadastroCli();
+                    cadastroCli.cadClientes();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    CadastroPro cadastroPro = new CadastroPro();
+                    cadastroPro.cadProdutos();
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    Console.WriteLine("\nOpção invalida tente novamente");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    exibirMenu();
+                    break;
+            }
+
+        }
+    }
+}
